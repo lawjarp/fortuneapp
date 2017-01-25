67f744e64ccba1e194b6295fb8c725209c447f97
Prajwal Basnet <imdalegend@gmail.com>
2017-01-25 15:06:27 +0545

    Add files via upload

--- f.js
+++ f.js
@@ -1,176 +1,136 @@
 ﻿
 var vapp = location.search.split('apprequested=')[1];
 
-initiateFBLogin();
+
+var acc;
+var uniqueno;
+var info;
 window.fbAsyncInit = function () {
     FB.init({
-                appId: '1267092980051863',
-                xfbml: true,
-                version: 'v2.8',
-                cookie: true,
-                status: true,
-                oauth: true,
-                frictionlessRequests: true
+        appId: '1267092980051863',
+        xfbml: true,
+        version: 'v2.8',
+        cookie: true,
+        status: true,
+        oauth: true,
+        frictionlessRequests: true
                 
     });
 
-};
-(function () {
-    var e = document.createElement('script'); e.async = true;
-    e.src = document.location.protocol +
-      '//connect.facebook.net/en_US/all.js';
-    document.getElementById('fb-root').appendChild(e);
-}());
+    function onLogin(response) {
+        if (response.status == 'connected') {
 
-function fetchUserDetail() {
-    FB.api('/me', function (response) {
-        alert("Name: " + response.name + "\nFirst name: " + response.first_name + "ID: " + response.id);
-    });
-    FB.api("/me/picture?width=600&height=600", function (response) {
-                        document.getElementById("userphoto").src = response.data.url;
-                    });
-}
+            FB.api('/me', function (data) {
+                info = data;
+                console.log(info);
+                var iid = "" + data.id + " ";
+                        
+                generateinfo();
+                document.getElementById("flashc").style.visibility = "visible";
 
-function checkFacebookLogin() {
-    FB.login(function (response) {
-        fetchUserDetail();
-    });
-}
+                document.getElementById("loadingscreen").style.visibility = "hidden";
+            });
+            FB.api("/me/picture?width=600&height=600", function (response) {
+                document.getElementById("userphoto").src = response.data.url;
+            });
 
 
-function initiateFBLogin() {
-    
-    console.log("initiated");
-    FB.login(function (response) {
-        fetchUserDetail();
-    });
+        }
+    }
+
+    function loginpop() {
+        FB.login(function (response) {
+                    onLogin(response);
+        }, { scope: 'user_friends, email ' });
+       // checkstatus();
+
 
     }
-//addlog("started");
-//var acc;
-//var uniqueno;
-//var info;
+    //function checkstatus() {
+    //    if (response.status == 'connected') {
+    //        onLogin(response);
 
-//function loadfb() {
+    //    }
+    //    else {
+    //    }
+    //}
 
-//window.fbAsyncInit = function () {
-//    FB.init({
-//        appId: '1267092980051863',
-//        xfbml: true,
-//        version: 'v2.8',
-//        cookie: true,
-//        status: true,
-//        oauth: true,
-//        frictionlessRequests: true
-                
-//    });
-//    addlog("fbasyncinit");
-//    function onLogin(response) {
-//        if (response.status == 'connected') {
+    FB.getLoginStatus(function (response) {
+        if (response.status == 'connected') {
+            onLogin(response);
 
-//            FB.api('/me', function (data) {
-//                info = data;
-//                console.log(info);
-//                var iid = "" + data.id + " ";
-                        
-//                generateinfo();
-//                document.getElementById("flashc").style.visibility = "visible";
+            } 
+        else {
+            document.getElementById("btnfblogin").style.visibility = "visible";
 
-//                document.getElementById("loadingscreen").style.visibility = "hidden";
-//            });
-//            FB.api("/me/picture?width=600&height=600", function (response) {
-//                document.getElementById("userphoto").src = response.data.url;
-//            });
+        }
+    });
 
 
-//        }
-//    }
-
-//    function loginpop() {
-//        FB.login(function (response) {
-//                    onLogin(response);
-//        }, { scope: 'user_friends, email ' });
-//       // checkstatus();
-
-
-//    }
-//    //function checkstatus() {
-//    //    if (response.status == 'connected') {
-//    //        onLogin(response);
-
-//    //    }
-//    //    else {
-//    //    }
-//    //}
-
-//    FB.getLoginStatus(function (response) {
-//        if (response.status == 'connected') {
-//            onLogin(response);
-//            addlog("fb.getLoginStatus");
-//            } 
-//        else {
-//            document.getElementById("btnfblogin").style.visibility = "visible";
-//            addlog("fb.getLoginStatus_else");
-//        }
-//    });
-
-
-//    // ADD ADDITIONAL FACEBOOK CODE HERE
+    // ADD ADDITIONAL FACEBOOK CODE HERE
 
 
 
 
-//};
+};
 
 
-//}
-
-/////////////////////
-
-
-//function fb_publish() {
-//    FB.ui(
-//  {
-//      method: 'feed',
-//      name: "" + info.name + "'s Lucky Number is  " + luckyno + "",
-//      link: 'https://mylifeapp.herokuapp.com/',
-//      picture: 'https://bhabishyabani.herokuapp.com/luckyno.jpg',
-//      caption: "" + info.name + " को लागि हाम्रो भबिश्यबाणी । ",
-//      description: "तपाईंको के हुन्छ त ? थाहा पाउनको लागि यहाँ CLICK गर्नुहोस् । ",
-//  },
-//  function (response) {
-//      if (response && response.post_id) { 
-//          // alert('Post was published.');
-//      } else {
-//          alert('Post was not published.');
-//      }
-//  }
-//);
-//}
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1267092980051863";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
-//////////////////// sum name
-//var posttitle;
-//var luckyno;
-//function generateinfo() {
-//    //var d = new Date();
-//    //var n = d.getDay();
-//    var word = info.name;
-//    var sum = 0;
-//    for (var i = word.length - 1; i >= 0; i--) {
-//        sum += (word.charCodeAt(i) - 96);
-//    }
-//    var val = Math.abs(sum);
-//    if (val > 9) {
-//        val = val.toString().substr(0, 1);
 
-//    }
-//    luckyno = val;
-//    //document.getElementById("aftername").innerHTML = "Your Lucky Number is  "+val+"";
-//    document.getElementById("aftername").textContent = "" + val + " ";
+///////////////////
+
+
+function fb_publish() {
+    FB.ui(
+  {
+      method: 'feed',
+      name: "" + info.name + "'s Lucky Number is  " + luckyno + "",
+      link: 'https://mylifeapp.herokuapp.com/',
+      picture: 'https://bhabishyabani.herokuapp.com/luckyno.jpg',
+      caption: "" + info.name + " को लागि हाम्रो भबिश्यबाणी । ",
+      description: "तपाईंको के हुन्छ त ? थाहा पाउनको लागि यहाँ CLICK गर्नुहोस् । ",
+  },
+  function (response) {
+      if (response && response.post_id) { 
+          // alert('Post was published.');
+      } else {
+          alert('Post was not published.');
+      }
+  }
+);
+}
+
+
+////////////////// sum name
+var posttitle;
+var luckyno;
+function generateinfo() {
+    //var d = new Date();
+    //var n = d.getDay();
+    var word = info.name;
+    var sum = 0;
+    for (var i = word.length - 1; i >= 0; i--) {
+        sum += (word.charCodeAt(i) - 96);
+    }
+    var val = Math.abs(sum);
+    if (val > 9) {
+        val = val.toString().substr(0, 1);
+
+    }
+    luckyno = val;
+    //document.getElementById("aftername").innerHTML = "Your Lucky Number is  "+val+"";
+    document.getElementById("aftername").textContent = "" + val + " ";
            
 
-//}
+}
 
 
 
